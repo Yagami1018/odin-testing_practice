5a0f0a2f088086d37fe4618b61581b310b7d01c3
XNAK3 <yagami01018@gmail.com>
2026-07-06 22:26:37 -0400

    Implement caesar Cipher #2

---
+++ src/tests/caesarCiph.test.js
@@ -0,0 +1,22 @@
+import { caesarCipher } from "../modules/utils";
+
+class cipherTest {
+    constructor(input, traslation, expected) {
+        this.input = input;
+        this.traslation = traslation;
+        this.expected = expected;
+        this.desc = `Caesar cipher "${input}" with ${traslation} to equals "${expected}".`;
+    }
+}
+
+const cipherTests = [
+    new cipherTest('Z',4,'D'),
+    new cipherTest("Hello", 3, "Khoor"),
+    new cipherTest("city", 3, "flwb"),
+];
+
+cipherTests.forEach((unit) => {
+    test(unit.desc, () => {
+        expect(caesarCipher(unit.input, unit.traslation)).toBe(unit.expected);
+    });
+});
